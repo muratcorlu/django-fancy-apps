e8d2e75be0577ccdf2432879607b9d23a9974ecd
Murat Çorlu <muratcorlu@gmail.com>
2012-01-18 01:13:25 +0200

    Mail template support added to mailform app

--- fancy/mailform/views.py
+++ fancy/mailform/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.template import RequestContext
+from django.template.loader import render_to_string
 from django.http import Http404, HttpResponse
 from django.conf import settings
 import settings as appsettings
@@ -9,12 +10,22 @@ if "mailer" in settings.INSTALLED_APPS:
 else:
     from django.core.mail import EmailMultiAlternatives
 
-def post_form(request):
-    message_html = ""
-    for key in request.POST:
+def get_mail_content(request):
+    tpl = 'default'
+    fields = {}
+    
+    for key, value in request.POST.items():
         if key[0] != '_': # ignore private fields
-            message_html += key + " : " + request.POST.get(key) + "<br />"
+            fields[key] = value
+    
+    if request.POST.get('_tpl'):
+        tpl = request.POST.get('_tpl')
+    
+    return render_to_string("fancy/mailform/mail/%s.html" % tpl, {'fields':fields})
 
+def post_form(request):
+    message_html = get_mail_content(request)
+    
     from_email = appsettings.FORM_FROM
     recipients = appsettings.FORM_RECIPIENTS
     redirect_to = request.POST.get('_success_url')
