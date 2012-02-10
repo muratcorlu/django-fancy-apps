f0830433c8b51e9a864dba2a1bf7885228dfe738
Murat Çorlu <muratcorlu@gmail.com>
2012-02-10 15:44:14 +0200

    logging issues for events app

--- fancy/events/models.py
+++ fancy/events/models.py
@@ -1,11 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
 class Location(models.Model):
     name = models.CharField(_("Location Name"), max_length=150)
     lat = models.FloatField(_("Location Latitude"))
     lon = models.FloatField(_("Location Longitude"))
+    created_date = models.DateTimeField(auto_now=True)
+    added_by = models.ForeignKey(User, verbose_name=_('Added by'),editable=False)
+    
+    def save(self, *args, **kwargs):
+        self.added_by = self.request.user
 
+        super(Location, self).save(*args, **kwargs) # Call the "real" save() method.
+    
     class Meta:
         verbose_name = _('Location')
         verbose_name_plural = _('Locations')
@@ -27,6 +35,13 @@ class Event(models.Model):
     location = models.ForeignKey(Location)
     description = models.TextField(_('Event Description'),blank=True)
     status = models.IntegerField(default=0,choices=EVENT_STATUSES)
+    created_date = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(User, verbose_name=_('Owner'),editable=False)
+    
+    def save(self, *args, **kwargs):
+        self.owner = self.request.user
+
+        super(Event, self).save(*args, **kwargs) # Call the "real" save() method.
     
     class Meta:
         verbose_name = _('Event')
