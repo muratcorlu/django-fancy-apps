f4778ef67cae1524d79ca9f50a75405a39f09e09
Murat Çorlu <muratcorlu@gmail.com>
2012-02-10 15:22:50 +0200

    Several fixes

--- fancy/events/urls.py
+++ fancy/events/urls.py
@@ -1,20 +1,6 @@
 from django.conf.urls.defaults import *
-from django.views.generic import DetailView, ListView
-from fancy.events.models import Event
 
-info_dict = {
-    'queryset': Event.objects.all(),
-    'template_object_name': 'events'
-}
-
-"""    (r'^$', 'django.views.generic.list_detail.object_list', info_dict),
-    (r'^(?P<object_id>\d+)/$', 'django.views.generic.list_detail.object_detail', info_dict),
-    url(r'^(?P<object_id>\d+)/results/$', 'django.views.generic.list_detail.object_detail', dict(info_dict, template_name='polls/results.html'), 'poll_results'),
-"""
-
-urlpatterns = patterns('',
-    (r'^$', ListView.as_view(
-                queryset=Event.objects.all(),
-                context_object_name='events_list',
-                template_name='events_list.html')),
+urlpatterns = patterns('fancy.events.views',
+    # events/12/:
+    url(r'^(?P<id>[\d]+)/$', 'event_detail', name='event_detail'),
 )
--- fancy/events/views.py
+++ fancy/events/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from models import Event
+
+def event_detail(request,id):
+    event = get_object_or_404(Event, id=id)
+    all_events = Event.objects.filter(status=1).order_by('-date_start')
+    return render_to_response("events/detail.html",{'event':event, 'all_events':all_events},context_instance=RequestContext(request))
