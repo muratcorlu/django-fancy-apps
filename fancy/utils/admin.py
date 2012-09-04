8c931eae942a137d5d228d2fd9e74e95efc26f6d
Murat Çorlu <muratcorlu@gmail.com>
2012-09-04 21:09:08 +0300

    Base models and admin settings updated

--- fancy/utils/admin.py
+++ fancy/utils/admin.py
@@ -1,12 +1,35 @@
 from django.contrib import admin
 from django.contrib.contenttypes import generic
 from models import Attribute, BaseModel
+from django.utils.translation import ugettext_lazy as _
 
 class MetaInline(generic.GenericTabularInline):
     model = Attribute
     extra = 0
 
 class BaseAdmin(admin.ModelAdmin):
+    def get_readonly_fields(self, request, obj=None):
+        fs = super(BaseAdmin, self).get_readonly_fields(request, obj)
+        fs += ('created_by', 'last_updated_by',)
+        return fs
+
+    def get_fieldsets(self, request, obj=None):
+        fs = super(BaseAdmin, self).get_fieldsets(request, obj)
+        
+        fs[0][1]['fields'].remove('created_by')
+        fs[0][1]['fields'].remove('last_updated_by')
+
+        fs.extend([(_('Other informations'), {'fields':['created_by','last_updated_by'], 'classes':['collapse']})])
+        return fs
+
+    """
+    def changelist_view(self, request, extra_context=None):
+        if request.user.has_perm('%s.can_view_deleted' % self.model._meta.app_label):
+            if not "deleted_flag" in self.list_filter:
+                self.list_filter += ("deleted_flag",)
+        return super(BaseAdmin, self).changelist_view(request, extra_context)
+    """
+    
     def save_model(self, request, obj, form, change):
         if not change:
             obj.created_by = request.user
