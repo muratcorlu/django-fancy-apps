baffb724d3352760d1cda4e822144ffa2c9fb8bd
Murat Çorlu <muratcorlu@gmail.com>
2012-09-06 22:47:26 +0300

    Product admin forms fix

--- fancy/products/admin_forms.py
+++ fancy/products/admin_forms.py
@@ -7,15 +7,22 @@ from models import Category, Product
 
 class CategoryForm(forms.ModelForm):
 
-    parent = TreeNodeChoiceField(queryset=Category.tree.all(), level_indicator=3*unichr(160), empty_label='---------', required=False)
+    parent = TreeNodeChoiceField(   verbose_name=_('Parent category'),
+                                    queryset=Category.tree.all(),
+                                    level_indicator=3*unichr(160),
+                                    empty_label='---------',
+                                    required=False)
 
     class Meta:
         model = Category
 
 class ProductForm(forms.ModelForm):
 
-    category = TreeNodeChoiceField(queryset=Category.tree.all(), level_indicator=3*unichr(160), empty_label='---------', required=False)
+    category = TreeNodeChoiceField( verbose_name=_('Category'),
+                                    queryset=Category.tree.all(),
+                                    level_indicator=3*unichr(160),
+                                    empty_label='---------',
+                                    required=True)
 
     class Meta:
         model = Product
-
