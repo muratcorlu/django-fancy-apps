95c58cdfb7dbc4553dcd3c3c4508d204e1883e27
Murat Çorlu <muratcorlu@gmail.com>
2015-11-25 17:54:12 +0200

    Fix showing unpublished posts Some refactoring

--- fancy/blog/models.py
+++ fancy/blog/models.py
@@ -9,21 +9,28 @@ from taggit.managers import TaggableManager
 import settings
 from django.core.urlresolvers import reverse
 
-class Post(MetadataModel,BaseModel):
-    title = models.CharField(_('Title'),max_length=200)
+
+class Post(MetadataModel, BaseModel):
+    title = models.CharField(_('Title'), max_length=200)
     slug = models.SlugField(_('Slug'), max_length=200, blank=True, unique=True)
     author = models.ForeignKey(User, on_delete=models.SET(get_sentinel_user))
     content = models.TextField(_('Post Content'), blank=True)
     date = models.DateTimeField(_('Publish Date'), default=datetime.now())
     enable_comments = models.BooleanField(default=True)
     categories = models.ManyToManyField('Category')
-    redirect_to = models.CharField(_('Redirect to'), help_text=_("Redirect this url to another url instead of showing"),blank=True,null=True,max_length=100)
+    redirect_to = models.CharField(_('Redirect to'),
+                                   help_text=_("Redirect this url to another url instead of showing"),
+                                   blank=True,
+                                   null=True,
+                                   max_length=100)
 
+    PUB_STATUS_DRAFT = 0
+    PUB_STATUS_PUBLISHED = 1
     PUB_STATUS = (
         (0, _('Draft')),
         (1, _('Published')),
     )
-    status = models.IntegerField(_('Status'),choices=PUB_STATUS, default=0)
+    status = models.IntegerField(_('Status'), choices=PUB_STATUS, default=PUB_STATUS_DRAFT)
 
     tags = TaggableManager(blank=True)
 
@@ -49,8 +56,8 @@ class Post(MetadataModel,BaseModel):
                 transaction.savepoint_rollback(savepoint)
                 i += 1
                 self.slug = '%s-%d' % (slug, i)
-
-        super(Post, self).save(*args, **kwargs) # Call the "real" save() method.
+        # Call the "real" save() method.
+        super(Post, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return self.title
@@ -65,10 +72,10 @@ class Post(MetadataModel,BaseModel):
         return self.get_absolute_url()
 
     def get_previous(self):
-        return self.get_previous_by_date(status__exact=1)
+        return self.get_previous_by_date(status__exact=self.PUB_STATUS_PUBLISHED)
 
     def get_next(self):
-        return self.get_next_by_date(status__exact=1)
+        return self.get_next_by_date(status__exact=self.PUB_STATUS_PUBLISHED)
 
     def get_content(self):
         if settings.BLOG_CONTENT_MARKDOWN:
@@ -76,6 +83,7 @@ class Post(MetadataModel,BaseModel):
             return Markdown.convert(self.content)
 
         return self.content
+
 
 class Category(MPTTModel,BaseModel):
     name = models.CharField(_('Category Name'), max_length=50)
@@ -92,13 +100,13 @@ class Category(MPTTModel,BaseModel):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('category_index', kwargs={'slug' : self.slug } )
+        return reverse('category_index', kwargs={'slug': self.slug})
 
     def get_last_post(self):
-        return Post.objects.filter(categories=self,status=1)[0]
+        return Post.objects.filter(categories=self, status=Post.PUB_STATUS_PUBLISHED)[0]
 
     def post_count(self):
-        return Post.objects.filter(categories=self,status=1).count()
+        return Post.objects.filter(categories=self, status=Post.PUB_STATUS_PUBLISHED).count()
 
     def last_post_date(self):
         return self.get_last_post().date
--- fancy/blog/views.py
+++ fancy/blog/views.py
@@ -4,66 +4,77 @@ from datetime import date
 from models import Post, Category
 from taggit.models import Tag
 
+
 def index(request):
-	category_list = Category.objects.all()
-	latest_posts = Post.objects.all()[:10]
+    category_list = Category.objects.all()
+    latest_posts = Post.objects.filter(status=Post.PUB_STATUS_PUBLISHED)[:10]
 
-	return render(request, 'fancy/blog/index.html', {
-		'category_list': category_list,
-		'latest_posts': latest_posts
-	})
-
-def detail(request,slug):
-	post = get_object_or_404(Post,slug=slug)
-
-	if post.redirect_to:
-	    return redirect(post.redirect_to)
-
-	return render(request, 'fancy/blog/detail.html', {'post':post})
-
-def show_post_list(request,post_list,title="",archieve_type="category"):
-	paginator = Paginator(post_list, 50)
-	page = int(request.GET.get('page', '1'))
-
-	try:
-		page = int(request.GET.get('page', '1'))
-	except ValueError:
-		page = 1
-
-	try:
-		posts = paginator.page(page)
-	except (EmptyPage, InvalidPage):
-		posts = paginator.page(paginator.num_pages)
-
-	return render(request, 'fancy/blog/posts.html', {
-						'post_list' : posts,
-						'title' : title,
-						'archieve_type' : archieve_type
-						})
-
-def tag_index(request,slug):
-	tag = get_object_or_404(Tag,slug=slug)
-	post_list = Post.objects.filter(tags__slug__in=[slug.lower()],status=1)
-	title = tag.name
-	return show_post_list(request,post_list,title,'tag')
-
-def category_index(request,slug):
-	category = get_object_or_404(Category,slug=slug)
-	post_list = Post.objects.filter(categories=category,status=1)
-	title = category.name
-	return show_post_list(request,post_list,title,'category')
-
-def year_index(request,year):
-	post_list=Post.objects.filter(date__year=year,status=1)
-	title = date( int(year), 1, 1)
-	return show_post_list(request,post_list,title,"year")
-
-def month_index(request,year,month):
-	post_list=Post.objects.filter(date__year=year, date__month=month,status=1)
-	title = date( int(year), int(month), 1)
-	return show_post_list(request,post_list,title,"month")
+    return render(request, 'fancy/blog/index.html', {
+        'category_list': category_list,
+        'latest_posts': latest_posts
+    })
 
 
+def detail(request, slug):
+    post = get_object_or_404(Post, slug=slug, status=Post.PUB_STATUS_PUBLISHED)
+
+    if post.redirect_to:
+        return redirect(post.redirect_to)
+
+    return render(request, 'fancy/blog/detail.html', {'post': post})
 
 
+def show_post_list(request, post_list, title="", archieve_type="category"):
+    paginator = Paginator(post_list, 50)
+    page = int(request.GET.get('page', '1'))
 
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
+
+    try:
+        posts = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        posts = paginator.page(paginator.num_pages)
+
+    return render(
+        request,
+        'fancy/blog/posts.html',
+        {
+            'post_list': posts,
+            'title': title,
+            'archieve_type': archieve_type
+        }
+    )
+
+
+def tag_index(request, slug):
+    tag = get_object_or_404(Tag, slug=slug)
+    post_list = Post.objects.filter(tags__slug__in=[slug.lower()],
+                                    status=Post.PUB_STATUS_PUBLISHED)
+    title = tag.name
+    return show_post_list(request, post_list, title, 'tag')
+
+
+def category_index(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    post_list = Post.objects.filter(categories=category,
+                                    status=Post.PUB_STATUS_PUBLISHED)
+    title = category.name
+    return show_post_list(request, post_list, title, 'category')
+
+
+def year_index(request, year):
+    post_list = Post.objects.filter(date__year=year,
+                                    status=Post.PUB_STATUS_PUBLISHED)
+    title = date(int(year), 1, 1)
+    return show_post_list(request, post_list, title, "year")
+
+
+def month_index(request, year, month):
+    post_list = Post.objects.filter(date__year=year,
+                                    date__month=month,
+                                    status=Post.PUB_STATUS_PUBLISHED)
+    title = date(int(year), int(month), 1)
+    return show_post_list(request, post_list, title, "month")
